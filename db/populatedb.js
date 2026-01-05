17e7b99c2bed2b13f9e01edcc8bbfb9324482556
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-01-05 12:57:31 +0530

    feat:improve database populating script and method Make db connection string to be available in .env file and make populatedb.js script to populate db based on arg provided during execution of file

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,13 +1,40 @@
 #! /usr/bin/env node
+const { argv } = require("node:process");
+require("dotenv").config();
 
 const { Client } = require("pg");
 
-const SQL = `CREATE TABLE IF NOT EXISTS books (
+const SQL = `CREATE TABLE IF NOT EXISTS genre (
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-book_name VARCHAR (255)
+name VARCHAR(255)
 );
 
-INSERT INTO books (book_name) VALUES
-('Atomic Habits'),
-('')
-`;
+CREATE TABLE IF NOT EXISTS book (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name VARCHAR (255),
+author VARCHAR (255),
+published_date DATE,
+description TEXT,
+genre_id INTEGER,
+FOREIGN KEY (genre_id) REFERENCES genre(id)
+)`;
+
+const connectionString =
+  argv[2] === "localdb"
+    ? process.env.LOCAL_DB_CONNECTION_STRING
+    : process.env.PRODUCTION_DB_CONNECTION_STRING;
+
+const main = async () => {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: connectionString,
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+
+  console.log("done");
+};
+
+main();
